96083d889f1992ebbe759d786f825c8e65506862
Revoltzin <jat1963@gmail.com>
2026-02-15 17:50:24 -0300

    Refactor import statement for expensesRoutes to use ES6 syntax

--- src/app.js
+++ src/app.js
@@ -1,9 +1,9 @@
 import express from 'express'
-const expensesRoutes = require('./routes/expenses.routes.js')
+import expensesRoutes from './routes/expenses.routes.js'
 const app = express()
 app.use(express.json())
-
 app.use('/expenses', expensesRoutes)
+
 
 app.listen(3000, () => {
     console.log("Server is running!")
