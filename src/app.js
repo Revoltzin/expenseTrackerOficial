90a44c7fa9b25837a950491d33e6874a1d099e81
Revoltzin <jat1963@gmail.com>
2026-02-15 17:15:56 -0300

    Imported the expensesRoutes into app

--- src/app.js
+++ src/app.js
@@ -1,6 +1,9 @@
 import express from 'express'
+const expensesRoutes = require('./routes/expenses.routes.js')
 const app = express()
 app.use(express.json())
+
+app.use('/expenses', expensesRoutes)
 
 app.listen(3000, () => {
     console.log("Server is running!")
